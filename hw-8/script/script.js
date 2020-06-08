bf22d0e7e950764c1db948cb25481bfba833f8f6
Marienko1 <53603619+Marienko1@users.noreply.github.com>
2020-06-08 15:13:55 +0300

    Add files via upload
    NEW

--- hw-8/script/script.js
+++ hw-8/script/script.js
@@ -4,7 +4,7 @@ const gallery = document.querySelector(".js-gallery");
 const lightBox = document.querySelector("div.lightbox");
 const lightBoxImg = document.querySelector("img.lightbox__image");
 const closeButton = document.querySelector(
-    'button[data-action="close-lightbox"]'
+  'button[data-action="close-lightbox"]'
 );
 const galleryItem = gallery.children;
 
@@ -12,9 +12,9 @@ let index;
 let imgSrc;
 
 galleryItems.forEach(data =>
-    gallery.insertAdjacentHTML(
-        "beforeend",
-        `<li class="gallery__item">
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery__item">
     <a
     class="gallery__link"
     href='${data.original}'
@@ -27,73 +27,73 @@ galleryItems.forEach(data =>
       />
     </a>
   </li>`
-    )
+  )
 );
 
 for (let i = 0; i < galleryItem.length; i++) {
-    galleryItem[i].addEventListener("click", event => {
-        event.preventDefault();
-        index = i;
-        changeImage();
-        openImage();
-    });
+  galleryItem[i].addEventListener("click", event => {
+    event.preventDefault();
+    index = i;
+    changeImage();
+    openImage();
+  });
 }
 
 function openImage() {
-    lightBox.classList.add("is-open");
+  lightBox.classList.add("is-open");
 }
 
 function changeImage() {
-    imgSrc = galleryItem[index].querySelector("img").getAttribute("data-source");
-    lightBoxImg.src = imgSrc;
+  imgSrc = galleryItem[index].querySelector("img").getAttribute("data-source");
+  lightBoxImg.src = imgSrc;
 }
 
 function closeModalWindow() {
-    lightBoxImg.src = "";
-    lightBox.classList.remove("is-open");
+  lightBoxImg.src = "";
+  lightBox.classList.remove("is-open");
 }
 
 document
-    .querySelector("div.lightbox__content")
-    .addEventListener("click", event => {
-        if (event.currentTarget !== event.target) {
-            return;
-        }
-        closeModalWindow();
-    });
+  .querySelector("div.lightbox__content")
+  .addEventListener("click", event => {
+    if (event.currentTarget !== event.target) {
+      return;
+    }
+    closeModalWindow();
+  });
 
 closeButton.addEventListener("click", closeModalWindow);
 
 document.onkeydown = event => {
-    if (imgSrc) {
-        if (event.code == "ArrowRight") {
-            next();
-        }
-        if (event.code == "ArrowLeft") {
-            prev();
-        }
-        if (event.code == "Escape") {
-            closeModalWindow();
-        }
-    } else {
-        return;
+  if (imgSrc) {
+    if (event.code == "ArrowRight") {
+      next();
     }
+    if (event.code == "ArrowLeft") {
+      prev();
+    }
+    if (event.code == "Escape") {
+      closeModalWindow();
+    }
+  } else {
+    return;
+  }
 };
 
 function next() {
-    if (index == galleryItem.length - 1) {
-        index = 0;
-    } else {
-        index++;
-    }
-    changeImage();
+  if (index == galleryItem.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  changeImage();
 }
 
 function prev() {
-    if (index == 0) {
-        index = galleryItem.length - 1;
-    } else {
-        index--;
-    }
-    changeImage();
+  if (index == 0) {
+    index = galleryItem.length - 1;
+  } else {
+    index--;
+  }
+  changeImage();
 }
